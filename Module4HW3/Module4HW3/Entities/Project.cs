b4b08a08da2ba73db8e1efb4192b093e560d300b
AntSenokosov <a.y.senokosov@gmail.com>
2022-01-19 17:07:37 +0200

    Add entity Project

---
+++ Module4HW3/Module4HW3/Entities/Project.cs
@@ -0,0 +1,9 @@
+﻿namespace Module4HW3.Entities;
+
+public class Project
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Budget { get; set; }
+    public DateTime StartedDate { get; set; }
+}
